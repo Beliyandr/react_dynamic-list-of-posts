a5aa69ec1e715a2e1387aecf349e198eee79a98a
beliyandr <beliyandr@gmail.com>
2025-09-14 23:09:49 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -8,53 +8,84 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
+import { User } from './types/User';
+import { Post } from './types/Post';
+import { getUsers } from './components/services/users';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [hasPosts, setHasPosts] = useState(false);
 
-              <Loader />
+  const [isLoading, setIsLoading] = useState(false);
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+  const [activeUser, setActiveUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUsers()
+      .then(users => setUsers(users))
+      .catch(error => {
+        setErrorMessage(error);
+      })
+      .finally(() => {});
+  }, []);
+
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  users={users}
+                  activeUser={activeUser}
+                  setActiveUser={setActiveUser}
+                />
               </div>
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
+              <div className="block" data-cy="MainContent">
+                {!activeUser && (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                )}
 
-              <PostsList />
+                <Loader />
+
+                <div
+                  className="notification is-danger"
+                  data-cy="PostsLoadingError"
+                >
+                  Something went wrong!
+                </div>
+
+                <div className="notification is-warning" data-cy="NoPostsYet">
+                  No posts yet
+                </div>
+
+                <PostsList />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
-          </div>
+          {/* <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              'Sidebar--open',
+            )}
+          >
+            <div className="tile is-child box is-success ">
+              <PostDetails />
+            </div>
+          </div> */}
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { User } from '../types/User';
+import classNames from 'classnames';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  users: User[];
+  setActiveUser: (user: User) => void;
+  activeUser: User | null;
+};
+
+export const UserSelector: FC<Props> = ({
+  users,
+  setActiveUser,
+  activeUser,
+}) => {
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
+
+  // const [selectedValue, setSelectedValue] = useState('Choose a user');
+
+  const handleActiveUser = (user: User) => {
+    setActiveUser(user);
+    setIsDropdownActive(!isDropdownActive);
+  };
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
+    <div
+      data-cy="UserSelector"
+      className={classNames('dropdown', { 'is-active': isDropdownActive })}
+    >
       <div className="dropdown-trigger">
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setIsDropdownActive(!isDropdownActive)}
         >
-          <span>Choose a user</span>
+          <span>{activeUser?.name ?? 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +45,21 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {users.map(user => {
+            const { id, name } = user;
+            return (
+              <a
+                key={id}
+                onClick={() => handleActiveUser(user)}
+                href={`#user-${id}`}
+                className={classNames('dropdown-item', {
+                  'is-active': id === activeUser?.id,
+                })}
+              >
+                {name}
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
